228788c1f43029e0e2e1b3020cf8d62e61cf3791
VStorm <tranvubao2004@gmail.com>
2025-07-14 16:36:12 +0700

    fix error experience

--- src/layouts/MainLayout/ExperienceSection/ExperienceSection.jsx
+++ src/layouts/MainLayout/ExperienceSection/ExperienceSection.jsx
@@ -4,39 +4,39 @@ import { experiences } from '~/configs/ExperiencesConfig';
 
 function ExperienceSection() {
     return (
-        <section id="experience" class="section">
-            <div class="elementor-spacer-inner-background">
-                <div class="elementor-spacer-inner space3"></div>
+        <section id="experience" className="section">
+            <div className="elementor-spacer-inner-background">
+                <div className="elementor-spacer-inner space3"></div>
             </div>
 
-            <h1 class="title-section">exp.</h1>
-            <div class="box-exp">
+            <h1 className="title-section">exp.</h1>
+            <div className="box-exp">
                 {experiences.map((experience, index) => (
-                    <div class="detail-box-exp text-main light">
-                        <div class="time-detail-box-exp">
-                            <div class="spe-box-exp">
-                                <div class="spe1-exp spe-exp"></div>{' '}
-                                <h4 class="from-time-detail-box-exp">{experience.from}</h4>{' '}
-                                <div class="spe2-exp spe-exp"></div>
+                    <div key={index} className="detail-box-exp text-main light">
+                        <div className="time-detail-box-exp">
+                            <div className="spe-box-exp">
+                                <div className="spe1-exp spe-exp"></div>{' '}
+                                <h4 className="from-time-detail-box-exp">{experience.from}</h4>{' '}
+                                <div className="spe2-exp spe-exp"></div>
                             </div>
-                            <div class="spe-box-exp">
-                                <div class="spe2-exp spe-exp"></div>{' '}
-                                <h4 class="to-time-detail-box-exp">{experience.to}</h4>{' '}
-                                <div class="spe1-exp spe-exp"></div>
+                            <div className="spe-box-exp">
+                                <div className="spe2-exp spe-exp"></div>{' '}
+                                <h4 className="to-time-detail-box-exp">{experience.to}</h4>{' '}
+                                <div className="spe1-exp spe-exp"></div>
                             </div>
                         </div>
 
-                        <div class="content-box-exp">
-                            <div class="block-content-box-exp">
-                                <p class="name-company-box-exp">
+                        <div className="content-box-exp">
+                            <div className="block-content-box-exp">
+                                <p className="name-company-box-exp">
                                     {experience.name_company}
-                                    <span class="fullname-company-box-exp">{experience.full_name}</span>
+                                    <span className="fullname-company-box-exp">{experience.full_name}</span>
                                 </p>
-                                <p class="location-company-box-exp">{experience.location}</p>
+                                <p className="location-company-box-exp">{experience.location}</p>
                             </div>
-                            <div class="block-content-box-exp">
-                                <p class="job-box-exp">{experience.job}</p>
-                                <p class="description">{experience.des_job}</p>
+                            <div className="block-content-box-exp">
+                                <p className="job-box-exp">{experience.job}</p>
+                                <p className="description">{experience.des_job}</p>
                             </div>
                         </div>
                     </div>
